3468f9a13bb12e1361d33a99f73d1ce0074b2dfb
James Bullard <jbullard@pacificbiosciences.com>
2012-05-16 15:22:27 -0700

    Small bug fixes to utils / modified description.

--- R/utils.R
+++ R/utils.R
@@ -392,9 +392,15 @@ readAlignmentSummaryGFF <- function(gffFile, keepAttributes = TRUE) {
   return(a)
 }
 
+namedRange <- function(range, names) {
+  names(range) <- names
+  return(range)
+}
 
-collapse <- function(x) {
+collapse <- function(x, colname = NA) {
   stopifnot(is.list(x))
+
+  x <- Filter(function(z) !is.null(z), x)
   
   if (! is.null(dim(x[[1]]))) {
     lf <- nrow
@@ -408,12 +414,16 @@ collapse <- function(x) {
   nx <- rep(names(x), sapply(x, lf))
   dx <- as.data.frame(do.call(cf, x), row.names = NULL)
 
-  i <- 1
-  while(paste("L", i, sep = "") %in% on) { 
-    i <- i + 1
+  if (is.na(colname)) {
+    i <- 1
+    while(paste("L", i, sep = "") %in% on) { 
+      i <- i + 1
+    }
+    ncname <- paste("L", i, sep = "")
+  } else {
+    if (colname %in% on) warning("colname already exists, setting anyway.")
+    ncname <- colname
   }
-  ncname <- paste("L", i, sep = "")
-  
   dx[[ncname]] <- nx
   colnames(dx) <- c(on, ncname)
   rownames(dx) <- NULL
@@ -426,4 +436,42 @@ toPhred <- function(ep, max) {
 
 fromPhred <- function(qv, max) {
   1/(10^(qv/10))
+}
+
+readSam <- function(fileName, ...) {
+  ## columns.
+  ##
+  ## 1	QNAME	Query template/pair NAME
+  ## 2	FLAG	bitwise FLAG
+  ## 3	RNAME	Reference sequence NAME
+  ## 4	POS     1-based leftmost POSition/coordinate of clipped sequence
+  ## 5	MAPQ	MAPping Quality (Phred-scaled)
+  ## 6	CIAGR	extended CIGAR string
+  ## 7	MRNM	Mate Reference sequence NaMe (‘=’ if same as RNAME)
+  ## 8	MPOS	1-based Mate POSistion
+  ## 9	TLEN	inferred Template LENgth (insert size)
+  ## 10	SEQ     query SEQuence on the same strand as the reference
+  ## 11	QUAL	query QUALity (ASCII-33 gives the Phred base quality)
+  ## 12+  OPT     variable OPTional fields in the format TAG:VTYPE:VALUE
+
+  ## bitmasks.
+  ##
+  ## 0x0001	p	the read is paired in sequencing
+  ## 0x0002	P	the read is mapped in a proper pair
+  ## 0x0004	u	the query sequence itself is unmapped
+  ## 0x0008	U	the mate is unmapped
+  ## 0x0010	r	strand of the query (1 for reverse)
+  ## 0x0020	R	strand of the mate
+  ## 0x0040	1	the read is the first read in a pair
+  ## 0x0080	2	the read is the second read in a pair
+  ## 0x0100	s	the alignment is not primary
+  ## 0x0200	f	the read fails platform/vendor quality checks
+  ## 0x0400	d	the read is either a PCR or an optical duplicate
+  l <- length(grep("^@", readLines(fileName)))
+  samFile <- read.table(fileName, skip = l, fill = T, stringsAsFactors = FALSE)
+
+  colnames(samFile) <- c('QNAME', 'FLAG', 'RNAME', 'POS', 'MAPQ', 'CIAGR', 'MRNM',
+                         'MPOS', 'TLEN', 'SEQ', 'QUAL',
+                         paste("OPT", 1:(ncol(samFile) - 11), sep = "_"))
+  return(samFile)
 }
